150d5f859cf31ae280c33ca7804dc2f314d7d415
ArtExpress <105834340+ArtExpress@users.noreply.github.com>
2022-10-21 18:33:10 -0700

    initial commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -25,7 +25,7 @@ function Header() {
         <Image 
             className='object-contain'
             src="https://links.papareact.com/ua6"
-            width={200} height={100}
+            width={200} height={100} alt=""
         />
     </header>
   )
--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -16,6 +16,7 @@ const Thumbnail = forwardRef(({ result }, ref) => {
                  }
             height={1080}
             width={1920}
+            alt=""
         />
 
         <div className='p-2'>
@@ -35,4 +36,5 @@ const Thumbnail = forwardRef(({ result }, ref) => {
   )
 })
 
+Thumbnail.displayName = "Thumbnail"
 export default Thumbnail
